21d28889fe62a48a64d12edb01c426c0a52e86e8
Nikita Aristarkhov <aristarhovnikita1@gmail.com>
2024-07-11 15:11:21 +0700

    feat: add clear completed and filters logic

--- src/componenets/footer/footer.js
+++ src/componenets/footer/footer.js
@@ -3,13 +3,15 @@ import "./footer.css";
 import TasksFilter from "../tasks-filter";
 export default class Footer extends Component {
   render() {
-    const { todo } = this.props;
+    const { todo, onClear, onChangeFilter } = this.props;
 
     return (
       <footer className="footer">
         <span className="todo-count">{todo} items left</span>
-        <TasksFilter />
-        <button className="clear-completed">Clear completed</button>
+        <TasksFilter onChangeFilter={onChangeFilter} />
+        <button className="clear-completed" onClick={onClear}>
+          Clear completed
+        </button>
       </footer>
     );
   }
